db501c439b205f306933110c34b6d8d65dc33364
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-04-03 13:16:22 -0500

    initiaze

---
+++ components/Online.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-if="online">Conexion exitosa</p>
+    <p v-else>sin conexion</p>
+    <p v-if="error != ''">{{error}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      online: false,
+      error: ''
+    }
+  },
+  async mounted () {
+    try {
+    const request = await  this.$axios.get('status', {})
+    this.online = request.data.online
+    console.log(request)
+    console.log(process.env.NODE_ENV)
+    } catch (error) {
+      this.error = error
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <Online />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
